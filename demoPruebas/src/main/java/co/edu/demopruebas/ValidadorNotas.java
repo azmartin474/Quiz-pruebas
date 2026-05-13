4246f3c3507f3855547d40335767322fcf7132aa
AzNitro474 <azmartin474@gmail.com>
2026-05-13 09:45:24 -0500

    Crear el paquete y la clase principal

---
+++ demoPruebas/src/main/java/co/edu/demopruebas/ValidadorNotas.java
@@ -0,0 +1,27 @@
+package co.edu.demopruebas;
+
+public class ValidadorNotas {
+
+    public boolean esAprobado(double nota) {
+        validarRango(nota);
+        return nota >= 3.0;
+    }
+
+    public String clasificarNota(double nota) {
+        validarRango(nota);
+
+        if (nota < 3.0) {
+            return "Reprobado";
+        } else if (nota < 4.0) {
+            return "Aprobado";
+        } else {
+            return "Sobresaliente";
+        }
+    }
+
+    public void validarRango(double nota) {
+        if (nota < 0.0 || nota > 5.0) {
+            throw new IllegalArgumentException("La nota debe estar entre 0.0 y 5.0");
+        }
+    }
+}
